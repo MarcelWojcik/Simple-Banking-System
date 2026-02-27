d75667674e015cf6dfd86b0126f1bf06740e7cee
Marcel Wójcik <marcelw.dev@gmail.com>
2026-02-27 13:05:10 +0100

    Added connecting to a database specified in cli arguments, initializing database and adding records.

--- src/banking/Account.java
+++ src/banking/Account.java
@@ -1,10 +1,15 @@
 package banking;
 
 
-import javax.smartcardio.Card;
+
 import java.lang.StringBuilder;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Random;
+
+import static banking.Main.dataSource;
 
 public class Account {
     private final static String BIN = "400000";
@@ -14,22 +19,34 @@ public class Account {
     public static HashMap<String, Account> Cards = new HashMap<>();
 
 
-    Account() {
+    Account() throws SQLException {
 
         this.pin = generateSequence(4);
         this.balance = 0;
 
-//        do{
-//
-//
-//        } while(Cards.containsKey(this.cardNumber));
         String sequence;
         do {
             sequence = this.BIN + generateSequence(9);
             sequence = sequence + generateLastDigit(sequence);
         } while(Cards.containsKey(sequence));
         this.cardNumber = sequence;
+
         Cards.put(this.cardNumber, this);
+
+
+        try(Connection conn = dataSource.getConnection()){
+
+            try(Statement statement = conn.createStatement()) {
+                statement.executeUpdate(String.format( """
+                            INSERT INTO card (number, pin) VALUES ("%s", "%s");
+                            """, this.cardNumber, this.pin)
+                );
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
--- src/banking/Main.java
+++ src/banking/Main.java
@@ -1,11 +1,57 @@
 package banking;
 
+import org.sqlite.SQLiteDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.JCommander;
+
 
 public class Main {
 
+    @Parameter(names = {"-fileName"}, description = "Database file name")
+    String fileName;
+
+    static SQLiteDataSource dataSource = new SQLiteDataSource();
+
+
+
     public static void main(String[] args){
 
+        init(args);
         Menu.start();
+
+    }
+
+    public static void init(String[] args) {
+
+        Main main = new Main();
+        JCommander.newBuilder().addObject(main).build().parse(args);
+
+
+        String url = "jdbc:sqlite:" + main.fileName;
+        dataSource.setUrl(url);
+
+        try(Connection con = dataSource.getConnection()){
+            try(Statement statement = con.createStatement()){
+                statement.executeUpdate(
+                        """
+                                CREATE TABLE IF NOT EXISTS card (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                number VARCHAR(16) UNIQUE,
+                                pin VARCHAR(4) NOT NULL,
+                                balance INTEGER DEFAULT 0
+                                );
+                            """
+                );
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
